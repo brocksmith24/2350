d85335ec83fa8493dabb9ffcfbf9dadafef7b603
brocksmith24 <brocksmith38@gmail.com>
2016-11-23 21:42:36 -0700

    Initial commit

---
+++ GPS Demo/payment.js
@@ -0,0 +1,30 @@
+/*
+
+   Author:   
+   Date:     
+
+   Filename: payment.js
+
+
+
+   Functions List:
+   startForm()
+      Sets up and initializes the form2 Web form.
+
+   checkForm3()
+      Checks the form to ensure that all required fields have been
+      entered by the user.
+
+   selectedCard()
+      Returns the index number of the selected credit card
+
+   checkNumber()
+      Returns a Boolean value indicating whether the credit card
+      number is valid
+
+   luhn()
+      Returns a Boolean value indicating whether the credit card
+      number satisfies the Luhn formula
+
+
+*/
